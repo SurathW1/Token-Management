de383b832abdc8bb45dee55d253fa19420bb4d4d
Surath Embuldeniya <160301857+SurathW1@users.noreply.github.com>
2024-10-30 01:06:36 +0530

    Add files via upload
    Initial Commit

---
+++ token-system/src/main/java/com/Surathw1/token_system/TokenSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Surathw1.token_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenSystemApplication.class, args);
+	}
+
+}
---
+++ token-system/src/test/java/com/Surathw1/token_system/TokenSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Surathw1.token_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
